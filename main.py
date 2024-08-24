234178d0a285443595ea17eee785727b3f726b49
Prizm <140013267+rPrizm@users.noreply.github.com>
2024-08-24 18:58:34 -0400

    added toggle between SAN and UCI format

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ coordDict = {'a': 0, 'b': 60, 'c': 120, 'd': 180,
 numberDict = {'1': 420, '2': 360, '3': 300,
               '4': 240, '5': 180, '6': 120, '7': 60, '8': 0}
 textBox = pygame.Rect(5, 485, 475, 25)
-toggleMode = pygame.Rect(5, 585, 50, 10)
+toggleMode = pygame.Rect(5, 575, 75, 20)
 
 piece_images = {
     'r': pygame.image.load("media/black_rook.png"),
@@ -34,12 +34,12 @@ piece_images = {
 }
 
 pygame.init()
-font = pygame.font.Font(None, 11)
+font = pygame.font.Font(None, 24)
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_icon(pygame.image.load("media/black_knight.png"))
 pygame.display.set_caption("Chess GUI")
 clock = pygame.time.Clock()
-inputMode = "uci"
+inputMode = "san"
 # Variable to hold the user input
 user_input = ""
 textBoxIsActive = False  # Track if text box is active
@@ -74,11 +74,16 @@ def main():
                     user_input = ""
 
                     print(userMove)
-                    if is_legal_move(board, userMove):
-                        print("move was legal")
-                        board.push_uci(userMove)
-                        gameOverCheck(board)
-
+                    if inputMode == "san":
+                        if is_legal_san_move(board, userMove):
+                            board.push_san(userMove)
+                        else:
+                            print("illegal move, try again")
+                    elif inputMode == "uci":
+                        if is_legal_uci_move(board, userMove):
+                            board.push_uci(userMove)
+                        else:
+                            print("illegal move, try again")
                 else:
                     # Add character to user input
                     user_input += event.unicode
@@ -94,11 +99,12 @@ def main():
         draw_text_on_rect(screen, user_input,
                           textBox, font, BLACK, WHITE)
         pygame.draw.rect(screen, WHITE, toggleMode)
-        draw_text_on_rect(screen, "toggle", toggleMode, font, BLACK, WHITE)
+        draw_text_on_rect(screen, inputMode,
+                          toggleMode, font, BLACK, WHITE)
         pygame.display.flip()
 
 
-def is_legal_move(board, uci_move):
+def is_legal_uci_move(board, uci_move):
     try:
         # Convert the UCI move string into a move object
         move = chess.Move.from_uci(uci_move)
@@ -110,6 +116,21 @@ def is_legal_move(board, uci_move):
             return False
     except:
         # If the UCI move format is invalid or can't be parsed
+        return False
+
+
+def is_legal_san_move(board, san_move):
+    try:
+        # Convert the SAN move to a Move object
+        move = board.parse_san(san_move)
+
+        # Check if the move is in the list of legal moves
+        if move in board.legal_moves:
+            return True
+        else:
+            return False
+    except ValueError:
+        # If the SAN move is invalid or can't be parsed
         return False
 
 
